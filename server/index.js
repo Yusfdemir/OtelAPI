ae06951149e9ce3a4782006baf8b2ed8cfa9c572
Yusfdemir <69212816+Yusfdemir@users.noreply.github.com>
2023-08-09 00:10:22 +0300

    serverla bağlantı kuruldu ve veri tabanı bağlantısı kuruldu

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express=require('express')
+const cors=require('cors')
+const cookieParser=require('cookie-parser')
+const bodyParser=require('body-parser')
+const dotenv=require('dotenv')
+const db = require('./config/db.js')
+
+dotenv.config();
+
+const app=express();
+app.use(cors())
+app.use(bodyParser.json({limit:'30mb', extended:true}))
+app.use(bodyParser.urlencoded({limit:'30mb', extended:true}))
+app.use(cookieParser)
+
+db();
+
+const PORT =5000;
+app.listen(PORT,()=>{
+    console.log("Server is running on port: ",PORT);
+})
